9b14be1f0f105cf783bed5910704f772e968c786
Renner McCreath Dev <rennermccreathdev@pop-os.localdomain>
2023-10-14 16:12:34 -0400

    II

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
 
-    <h1>coming soon...</h1>
+    <h1>rennermccreathdev will return...</h1>
     </>
   )
 }
