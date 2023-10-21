89013140f3e03a616fe7fc6f42e7c0777450d8d2
Renner McCreath Dev <rennermccreathdev@pop-os.localdomain>
2023-10-21 16:04:41 -0400

    update before experimentation

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div class="logoletters">
+      <h1 class="logoR">R</h1>
+      <h1 class="logoM">M</h1>
+    </div>
+  );
+};
+
+export default Logo;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,13 +3,15 @@ import Navbar from "../../components/Navbar";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import Home from "../../components/Home";
+import Logo from "../../components/Logo";
 import "./homepage.css"
 
 const HomePage = () => {
     return (
         <div>
-            {/* <Navbar /> */}
+            <Navbar />
             <Header />
+            <Logo />
             <div className="flexy">
             <Title />
             <Home />
