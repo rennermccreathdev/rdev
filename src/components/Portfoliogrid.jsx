de58a62510527808a7d12b4d92336383f7f353f1
Renner McCreath Dev <rennermccreathdev@gmail.com>
2023-10-27 16:57:13 -0400

    adding components

---
+++ src/components/Portfoliogrid.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./componentstyle.css"
+
+const Portfoliogrid = () => {
+  return (
+    <div>
+      <h2 class="logoletters small">Hi there.</h2>
+      <h3>I'm Renner - a full-stack software engineer based in Canada.</h3>
+      <h3>This page is currently a work in progress. Check back frequently for updates!</h3>  
+    </div>
+  );
+};
+
+export default Portfoliogrid;
